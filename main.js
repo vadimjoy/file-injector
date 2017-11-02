65879d9fa0df23ae1bd22722d9bc7fb8930e953c
vadimjoy <madbjoy@gmail.com>
2017-11-02 17:37:40 +0500

    fixed

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 var elem = document.querySelectorAll('.js-file-uploader')[0];
 var target = document.querySelectorAll('.js-target')[0];
-new FileUploader(elem, target, function (file) {
-    console.log(file)
+
+function imagePreview(info, image) {
+    /**
+     * While image not loaded get info about load process
+     */
+    console.log(info);
+
+    /**
+     * If image loaded append this in block
+     */
+    image ? target.appendChild(image) : null;
+}
+
+new FileUploader({elem: elem, imagePreview: imagePreview}, function (file) {
+    /**
+     * Get original file
+     */
+    console.log(file);
 });
