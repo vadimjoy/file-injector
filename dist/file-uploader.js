3b13ff600ddeb7364dbda4c15c9d578c34d287d7
Vadim <madbuster@yandex.ru>
2017-11-02 13:56:32 +0500

    fixed

--- dist/file-uploader.js
+++ dist/file-uploader.js
@@ -83,6 +83,11 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__css_common_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__css_common_css__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__css_main_css__ = __webpack_require__(2);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__css_main_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__css_main_css__);
+/**
+ * @license
+ * Copyright Vadim Joy. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 
 
 var FileUploader = /** @class */ (function () {
