8a34e34d405a5b5f0d19857aac9b880bb6a0faff
vadimjoy <madbjoy@gmail.com>
2017-11-02 20:28:15 +0500

    fixed

--- dist/file-injector.js
+++ dist/file-injector.js
@@ -198,28 +198,6 @@ var FileInjector = /** @class */ (function () {
     return FileInjector;
 }());
 /* harmony default export */ __webpack_exports__["default"] = (FileInjector);
-var elem = document.querySelectorAll('.js-file-uploader')[0];
-var target = document.querySelectorAll('.js-target')[0];
-function readStatus(status) {
-    /**
-     * While image not loaded get info about load process
-     */
-    console.log(status);
-}
-function imagePreview(base64) {
-    /**
-     * If image loaded append this in block
-     */
-    var image = new Image();
-    image.src = base64;
-    target.appendChild(image);
-}
-new FileInjector({ elem: elem, imagePreview: imagePreview, readStatus: readStatus }, function (file) {
-    /**
-     * Get original file
-     */
-    console.log(file);
-});
 
 
 /***/ }),
