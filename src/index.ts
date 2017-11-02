1a1b4932c28357ff5c85680398ba973c93ddb3d8
Zhoydik Vadim <vdj@napoleonit.ru>
2017-11-02 17:56:31 +0500

    fixed

--- src/index.ts
+++ src/index.ts
@@ -135,25 +135,3 @@ export default class FileUploader {
         }
     }
 }
-
-var elem = document.querySelectorAll('.js-file-uploader')[0];
-var target = document.querySelectorAll('.js-target')[0];
-
-function imagePreview(info:any, image:any) {
-    /**
-     * While image not loaded get info about load process
-     */
-    console.log(info);
-
-    /**
-     * If image loaded append this in block
-     */
-    image ? target.appendChild(image) : null;
-}
-
-new FileUploader({elem: elem, imagePreview: imagePreview}, function (file:any) {
-    /**
-     * Get o file
-     */
-    console.log(file);
-});
