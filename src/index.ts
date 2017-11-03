d204a4b3f89b36405aaea87a154d1e1d19141d09
Zhoydik Vadim <vdj@napoleonit.ru>
2017-11-03 15:52:56 +0500

    fixed

--- src/index.ts
+++ src/index.ts
@@ -136,3 +136,31 @@ export default class FileInjector {
         }
     }
 }
+
+var elem = document.querySelectorAll('.js-file-uploader')[0];
+var target = document.querySelectorAll('.js-target')[0];
+
+
+function readStatus(status:any) {
+    /**
+     * While image not loaded get info about load process
+     */
+    console.log(status);
+}
+
+function imagePreview(base64:any) {
+    /**
+     * If image loaded append this in block
+     */
+    var image = new Image();
+    image.src = base64;
+
+    target.appendChild(image);
+}
+
+new FileInjector({elem: elem, imagePreview: imagePreview, readStatus: readStatus}, function (file:any) {
+    /**
+     * Get original file
+     */
+    console.log(file);
+});
